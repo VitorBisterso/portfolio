80d87700f8585637824dad7496b8e822abb41e9d
Vitor Bisterso dos Santos <vitorbisterso@gmail.com>
2020-10-15 15:39:11 -0300

    Refactor Home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,37 +7,47 @@ import myself from '../../assets/img/me.jpg';
 
 import './index.css';
 
+const renderMenu = () => (
+  <div className="home-menu">
+    <ul>
+      <li>
+        <Translate tKey="header.professional" />
+      </li>
+      <li>
+        <Translate tKey="header.academic" />
+      </li>
+      <li>
+        <Translate tKey="header.about" />
+      </li>
+    </ul>
+  </div>
+);
+
+const renderHeader = () => (
+  <div className="home-header">
+    <div className="home-menuContainer">
+      {renderMenu()}
+      <div className="home-selectLanguage">
+        <TranslateDropdown />
+      </div>
+    </div>
+  </div>
+);
+
+const renderBody = () => (
+  <div className="home-body">
+    <div className="home-title">
+      <Translate tKey="header.title" />
+    </div>
+
+    <img alt="me" src={myself} className="home-myPicture" />
+  </div>
+);
+
 const Home = () => (
   <div className="home-container">
-    <div className="home-header">
-      <div className="home-menuContainer">
-        <div className="home-menu">
-          <ul>
-            <li>
-              <Translate tKey="header.professional" />
-            </li>
-            <li>
-              <Translate tKey="header.academic" />
-            </li>
-            <li>
-              <Translate tKey="header.about" />
-            </li>
-          </ul>
-        </div>
-
-        <div className="home-selectLanguage">
-          <TranslateDropdown />
-        </div>
-      </div>
-    </div>
-
-    <div className="home-body">
-      <div className="home-title">
-        <Translate tKey="header.title" />
-      </div>
-
-      <img alt="me" src={myself} className="home-myPicture" />
-    </div>
+    {renderHeader()}
+    {renderBody()}
   </div>
 );
 
