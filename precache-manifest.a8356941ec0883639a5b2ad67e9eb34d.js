89fefca218e7f4207efa17d91dfecfa19f265620
Vitor Bisterso dos Santos <vitorbisterso@gmail.com>
2020-10-16 15:08:29 -0300

    Updates

---
+++ precache-manifest.a8356941ec0883639a5b2ad67e9eb34d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0d3c731b8472bd6cf14adf93285ae3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c7bad509101218a8156b",
+    "url": "/portfolio/static/css/main.4a4f8c93.chunk.css"
+  },
+  {
+    "revision": "a3385dc18df3e7af2f4c",
+    "url": "/portfolio/static/js/2.4df50904.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.4df50904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7bad509101218a8156b",
+    "url": "/portfolio/static/js/main.17bcb37b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/portfolio/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "0e33334e1f25b9b7dd335064dfd067e7",
+    "url": "/portfolio/static/media/cotuca.0e33334e.png"
+  },
+  {
+    "revision": "c4a029068a48311107e58645fa3c73f4",
+    "url": "/portfolio/static/media/family.c4a02906.jpg"
+  },
+  {
+    "revision": "794cb208fba81e5f33c1ab811df20c98",
+    "url": "/portfolio/static/media/ifsp.794cb208.png"
+  },
+  {
+    "revision": "f889c91292c3855ba21e2aecf3ace13e",
+    "url": "/portfolio/static/media/me.f889c912.jpg"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/portfolio/static/media/react.580ffd26.png"
+  },
+  {
+    "revision": "89359892a63ee8d5ec093ea3890540f3",
+    "url": "/portfolio/static/media/teemo.89359892.png"
+  }
+]);
